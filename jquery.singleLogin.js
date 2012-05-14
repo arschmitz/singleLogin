dbf929e28dd7b0af2e1251a7a4ce6f67a50fdc1c
Alexander Schmitz <arschmitz@gmail.com>
2012-05-14 16:54:35 -0400

    clean up code a bit

--- jquery.singleLogin.js
+++ jquery.singleLogin.js
@@ -10,29 +10,28 @@
 						$.cookie($.singleLogin.options.cookie,null,{expires:$.singleLogin.options.life,path:'/',domain:$.singleLogin.options.root});
 						$.singleLogin.options.logout();
 						return null;
-						
 						break;
 					case "set":
 						setOptions(options);
 						$.cookie($.singleLogin.options.cookie,userid,{expires:$.singleLogin.options.life,path:'/',domain:$.singleLogin.options.root});
-							$.singleLogin.options.set();
-							return null;
+						$.singleLogin.options.set();
+						return null;
 						break;
 					case "check":
 						setOptions(options);
 						$.cookie($.singleLogin.options.cookie)
-							if($.cookie($.singleLogin.options.cookie) != null){
-								$.singleLogin.userid = $.cookie($.singleLogin.options.cookie);
-								$.singleLogin.options.success();
-							} else {
-								$.singleLogin.options.failure();
-								return null
-							}
-							$($.singleLogin.options.logoutClass).click(function(){
-								$.singleLogin('logout');
-								return null;
-							});
+						if($.cookie($.singleLogin.options.cookie) != null){
+							$.singleLogin.userid = $.cookie($.singleLogin.options.cookie);
+							$.singleLogin.options.success();
+						} else {
+							$.singleLogin.options.failure();
+							return null
+						}
+						$($.singleLogin.options.logoutClass).click(function(){
+							$.singleLogin('logout');
 							return null;
+						});
+						return null;
 				}
 			//merge user options with defaults and construct root
 			function setOptions(options){
@@ -61,7 +60,6 @@
 					},
 					cookie:'singleLogin',
 					forcePort:''
-					
     			};
     			options = $.extend({},defaults,options);
     			$.singleLogin.options=options;
@@ -78,22 +76,17 @@
 			}
     	},
 		cookie: function(key, value, options) {
-
         // key and at least value given, set cookie...
         if (arguments.length > 1 && (!/Object/.test(Object.prototype.toString.call(value)) || value === null || value === undefined)) {
             options = $.extend({}, options);
-
             if (value === null || value === undefined) {
                 options.expires = -1;
             }
-
             if (typeof options.expires === 'number') {
                 var days = options.expires, t = options.expires = new Date();
                 t.setDate(t.getDate() + days);
             }
-
             value = String(value);
-
             return (document.cookie = [
                 encodeURIComponent(key), '=', options.raw ? value : encodeURIComponent(value),
                 options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
@@ -102,11 +95,9 @@
                 options.secure  ? '; secure' : ''
             ].join(''));
         }
-
         // key and possibly options given, get cookie...
         options = value || {};
         var decode = options.raw ? function(s) { return s; } : decodeURIComponent;
-
         var pairs = document.cookie.split('; ');
         for (var i = 0, pair; pair = pairs[i] && pairs[i].split('='); i++) {
             if (decode(pair[0]) === key) return decode(pair[1] || ''); // IE saves cookies with empty string as "c; ", e.g. without "=" as opposed to EOMB, thus pair[1] may be undefined
