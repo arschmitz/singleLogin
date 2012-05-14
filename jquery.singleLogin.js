fa9ae48aa6a48cf29efdf19f69f2b39238d47e4d
Alexander Schmitz <arschmitz@gmail.com>
2012-05-14 16:36:05 -0400

    updated to remove dependency and use of helper

--- jquery.singleLogin.js
+++ jquery.singleLogin.js
@@ -2,63 +2,30 @@
 (function($) {
     $.extend({
     	singleLogin: function(role, options, userid){
-			var options = options||"undefined",
-				userid = userid||'';
+			var options = options||null,
+				userid = userid||null;
     			switch(role){
-					case "helper":
-						//get options from query string
-						$.singleLogin.options = $.parseJSON(decodeURI($.getQueryString('options')));
-						//if role is helper read action from hash
-						switch (window.location.hash){
-							case "#set":
-								document.domain = $.singleLogin.options.root;
-								$.cookie($.singleLogin.options.cookie,decodeURI($.getQueryString('userid')),{expires:$.singleLogin.options.life,path:'/'});
-								return null;
-								break;
-							case "#check":
-								document.domain = $.singleLogin.options.root;
-								if($.cookie($.singleLogin.options.cookie) != null){
-									window.parent.$.singleLogin.loggedin=true;
-									window.parent.$.singleLogin.userid = $.cookie($.singleLogin.options.cookie);
-									console.log();
-									return null;
-								} else {
-									window.parent.$.singleLogin.loggedin=false;
-									return null;
-								}
-								break;
-							case "#logout":
-								document.domain = $.singleLogin.options.root;
-								$.cookie($.singleLogin.options.cookie,null,{expires:$.singleLogin.options.life,path:'/'});
-								return null;
-								break;
-						}
-						break;
 					case "logout":
-						build('logout',options);
-						$('#singleLogin_frame').load(function(){
-							$.singleLogin.options.logout();
-							return null;
-						});
+						setOptions(options);
+						$.cookie($.singleLogin.options.cookie,null,{expires:$.singleLogin.options.life,path:'/',domain:$.singleLogin.options.root});
+						$.singleLogin.options.logout();
+						return null;
+						
 						break;
 					case "set":
-						build('set',options,userid);
-						
-						$('#singleLogin_frame').load(function(){
+						setOptions(options);
+						$.cookie($.singleLogin.options.cookie,userid,{expires:$.singleLogin.options.life,path:'/',domain:$.singleLogin.options.root});
 							$.singleLogin.options.set();
 							return null;
-						});
 						break;
 					case "check":
-						build('check',options);
-						$('#singleLogin_frame').load(function(){
-							if($.singleLogin.loggedin == true){
+						setOptions(options);
+						$.cookie($.singleLogin.options.cookie)
+							if($.cookie($.singleLogin.options.cookie) != null){
+								$.singleLogin.userid = $.cookie($.singleLogin.options.cookie);
 								$.singleLogin.options.success();
-								$('#singleLogin_frame').remove();
-								
 							} else {
 								$.singleLogin.options.failure();
-								$('#singleLogin_frame').remove();
 								return null
 							}
 							$($.singleLogin.options.logoutClass).click(function(){
@@ -66,24 +33,12 @@
 								return null;
 							});
 							return null;
-						});
 				}
-			function build(type,options,userid){
-				//check to see if singleLogin has already been initated if not build options 
-						if(typeof $.singleLogin.options === "undefined"){
-							setOptions(options);
-							document.domain = $.singleLogin.options.root;
-						}
-						$('body').append('<iframe id="singleLogin_frame" src="http://'+$.singleLogin.options.rootport+$.singleLogin.options.helper+'?options='+encodeURI(JSON.stringify($.singleLogin.options))+((typeof userid !== "undefined")?('&userid='+userid):(''))+'#'+type+'" style="display:none;"></iframe>');
-			}
 			//merge user options with defaults and construct root
 			function setOptions(options){
     			var defaults = {
-    				subdomain:'',
-    				basePort:'',
     				loginPage:'login.html',
     				logoutClass:'.logout',
-					helper:'helper.html',
 					life:7,
     				success:function(){
     					return null;
@@ -111,12 +66,11 @@
 				var root = "";
 				if(domainTest.test(document.domain)){
 					root = document.domain.split('.');
-					root = ((options.subdomain == '')?'':('.'+options.subdomain))+root[root.length-2]+'.'+root[root.length-1];
+					root = '.'+root[root.length-2]+'.'+root[root.length-1];
 				} else {
 					root = window.location.hostname;
 				}
 				$.singleLogin.options.root = root;
-				$.singleLogin.options.rootport = root+':'+options.basePort;
 				return true;
 			}
     	},
@@ -155,26 +109,6 @@
             if (decode(pair[0]) === key) return decode(pair[1] || ''); // IE saves cookies with empty string as "c; ", e.g. without "=" as opposed to EOMB, thus pair[1] may be undefined
         }
         return null;
-      },
-	  getQueryString: function (name) {           
-            function parseParams() {
-                var params = {},
-                    e,
-                    a = /\+/g,  // Regex for replacing addition symbol with a space
-                    r = /([^&=]+)=?([^&]*)/g,
-                    d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
-                    q = window.location.search.substring(1);
-
-                while (e = r.exec(q))
-                    params[d(e[1])] = d(e[2]);
-
-                return params;
-            }
-
-            if (!this.queryStringParams)
-                this.queryStringParams = parseParams(); 
-
-            return this.queryStringParams[name];
-        }
+      }
     });
 })(jQuery);
